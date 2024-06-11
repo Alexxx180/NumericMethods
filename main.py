092d227a08703d9ba1df845f412d42b9fbc94daf
Alexxx180 <alexx120702@gmail.com>
2024-06-11 21:39:35 +0300

    fix variant selection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import matplotlib
+from common.handlers.interaction import pause
 from common.handlers.input.specific import setup_input
 from common.commander.resources import Resources
 from start import dropdown, setup_menu
@@ -12,7 +13,10 @@ def resource(file):
             resources[key] = resource(value)
         return resources
     elif isinstance(file, list):
-        no: int = variant % (file[1] + 1)
+        start: int = 1
+        no: int = (variant - start) % file[1] + start
+        print(Resources.Texts["Common"]["Variant"].format(file[0], file[1], no))
+
         return Resources.at(f'resources/{file[0]}/{no}.json')
     else:
         return Resources.at(f'resources/{file}.json')
@@ -26,9 +30,13 @@ def main():
 
 if __name__ == '__main__':
     matplotlib.use('TkAgg')
+
     with open('variant.txt') as no:
         variant = int(no.readline())
+
+    print("Program No {0}, Variants check...\n".format(variant))
     main()
     setup_input(Resources.at('resources/texts/queries.json'))
     setup_menu()
+    pause()
     dropdown()
